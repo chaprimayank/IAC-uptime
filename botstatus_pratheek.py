2c3e24154b9ef9b266229914b71ac4a6e0255fd6
Mayank <94171814+chaprimayank@users.noreply.github.com>
2023-03-12 13:23:08 +0530

    Update botstatus_pratheek.py

--- botstatus_pratheek.py
+++ botstatus_pratheek.py
@@ -17,7 +17,7 @@ app = Client(
 )
 
 TIME_ZONE = 'Asia/Kolkata'
-BOT_LIST = 'Lena_MilizeBot, roronoa_zoro_robot, NamiMusic_Bot, NamiMusic2_Bot'
+BOT_LIST = 'Lena_MilizeBot, roronoa_zoro_robot, NamiMusic_Bot, NamiMusic2_Bot, NSFW_Protectionbot'
 CHANNEL_OR_GROUP_ID = '-1001557054615'
 MESSAGE_ID = 35
 BOT_ADMIN_IDS = [837914403, 1189238402]
@@ -51,7 +51,7 @@ async def main_pratheek():
                     await asyncio.sleep(e.x)
             time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
             last_update = time.strftime(f"%d %b %Y at %I:%M %p")
-            xxx_pratheek += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n**♻️ Refreshes automatically - Powered By Pratheek**"
+            xxx_pratheek += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n**♻️ Refreshes automatically - Powered By IAC Network**"
             await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_pratheek)
             print(f"Last checked on: {last_update}")
             await asyncio.sleep(6300)
